799c0f1d68466e5f9fccfa95f3a2819b8a31fcd2
turtlemurphy <turtlemurphy@gmail.com>
2021-12-02 20:54:59 -0600

    added chitchatpractice

---
+++ chitchatpractice/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+
+	//create new instance of the multiplexer
+	mux := http.NewServeMux()
+
+	//use the multplexer to serve a static file
+	files := http.FileServer(http.Dir("/public"))
+	//use the StripPrefix funtion to remove the given prefix from the request URLS's path
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html"}
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := data.Threads()
+	if err == nil {
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
+
+}
