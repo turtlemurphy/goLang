124278f4fea4316f6404a29138f0f50046484721
turtlemurphy <turtlemurphy@gmail.com>
2022-03-31 01:24:54 -0500

    foray into print formatting

--- values/values.go
+++ values/values.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 
 	val := 122
-
+	//The integer is formatted in decimal, hexadecimal, octal,
+	//and binary notations. It is also formatted as a character
+	// literal, quoted charracter literal, and in a Unicode escape sequence.
 	fmt.Printf("%d\n", val)
 	fmt.Printf("%c\n", val)
 	fmt.Printf("%q\n", val)
@@ -15,6 +17,44 @@ func main() {
 	fmt.Printf("%O\n", val)
 	fmt.Printf("%b\n", val)
 	fmt.Printf("%U\n", val)
+
+	fmt.Printf("%0.f\n", 16.540)
+	fmt.Printf("%0.2f\n", 16.540)
+	fmt.Printf("%0.3f\n", 16.540)
+	fmt.Printf("%0.5f\n", 16.540)
+
+	//f - decimal floating point, lowercase
+	//F - decimal floating point, uppercase
+	//e - scientific notation (mantissa/exponent), lowercase
+	//E - scientific notation (mantissa/exponent), uppercase
+	//g - uses the shortest representation of %e or %f
+	//G - Use the shortest representation of %E or %F
+
+	val1 := 1273.78888769000
+
+	fmt.Printf("%f\n", val1)
+	fmt.Printf("%e\n", val1)
+	fmt.Printf("%g\n", val1)
+	fmt.Printf("%E\n", val1)
+	fmt.Printf("%G\n", val1)
+
+	fmt.Println("-------------------------")
+
+	fmt.Printf("%.10f\n", val1)
+	fmt.Printf("%.10e\n", val1)
+	fmt.Printf("%.10g\n", val1)
+	fmt.Printf("%.10E\n", val1)
+	fmt.Printf("%.10G\n", val1)
+
+	fmt.Println("-------------------------")
+
+	val3 := 66_000_000_000.1200
+
+	fmt.Printf("%f\n", val3)
+	fmt.Printf("%e\n", val3)
+	fmt.Printf("%g\n", val3)
+	fmt.Printf("%E\n", val3)
+	fmt.Printf("%G\n", val3)
 
 	fmt.Printf("%d\n", 1671)
 	fmt.Printf("%o\n", 1671)
