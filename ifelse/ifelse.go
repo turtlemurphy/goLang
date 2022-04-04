077b4e2c57928cf5a87f31a4e16e6744d5d573c9
turtlemurphy <turtlemurphy@gmail.com>
2022-04-04 00:35:50 -0500

    experiments with if/else

--- ifelse/ifelse.go
+++ ifelse/ifelse.go
@@ -7,27 +7,34 @@ func main() {
 	fmt.Print("\n")
 	fmt.Print("\n")
 
-	if 7%2 == 0 {
-		fmt.Println("7 is even")
-		fmt.Print("\n")
-	} else {
-		fmt.Println("7 is odd")
-		fmt.Print("\n")
+	for i := 1; i <= 69; i++ {
+
+		if i%2 == 0 {
+			fmt.Println(i, " is even")
+			//fmt.Print("\n")
+		} else {
+			fmt.Println(i, " is odd")
+			//fmt.Print("\n")
+		}
+
+		if i%4 == 0 {
+			fmt.Println(i, " is divisible by 4")
+			//fmt.Print("\n")
+		}
+
+		if i < 0 {
+			fmt.Println(i, "is negative")
+			fmt.Print("\n")
+		} else if i < 10 {
+			fmt.Println(i, "has 1 digit")
+			fmt.Print("\n")
+		} else {
+			fmt.Println(i, "has multiple digits")
+			fmt.Print("\n")
+		}
+		// if i%20 == 0 {
+		// 	fmt.Print("\n")
+		// }
 	}
 
-	if 8%4 == 0 {
-		fmt.Println("8 is divisible by 4")
-		fmt.Print("\n")
-	}
-
-	if num := 9; num < 0 {
-		fmt.Println(num, "is negative")
-		fmt.Print("\n")
-	} else if num < 10 {
-		fmt.Println(num, "has 1 digit")
-		fmt.Print("\n")
-	} else {
-		fmt.Println(num, "has multiple digits")
-		fmt.Print("\n")
-	}
 }
