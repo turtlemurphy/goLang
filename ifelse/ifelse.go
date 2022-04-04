66c347b308dbb879e2cb35a30bf9447402db1610
turtlemurphy <turtlemurphy@gmail.com>
2022-04-04 00:46:17 -0500

    experiments with if/else

--- ifelse/ifelse.go
+++ ifelse/ifelse.go
@@ -7,30 +7,30 @@ func main() {
 	fmt.Print("\n")
 	fmt.Print("\n")
 
-	for i := 1; i <= 69; i++ {
+	for i := -100; i <= 100; i += 10 {
 
 		if i%2 == 0 {
 			fmt.Println(i, " is even")
-			//fmt.Print("\n")
+
 		} else {
 			fmt.Println(i, " is odd")
-			//fmt.Print("\n")
+
 		}
 
 		if i%4 == 0 {
 			fmt.Println(i, " is divisible by 4")
-			//fmt.Print("\n")
+
 		}
 
 		if i < 0 {
-			fmt.Println(i, "is negative")
-			fmt.Print("\n")
+			fmt.Println(i, " is negative")
+
 		} else if i < 10 {
-			fmt.Println(i, "has 1 digit")
-			fmt.Print("\n")
+			fmt.Println(i, " has 1 digit")
+
 		} else {
-			fmt.Println(i, "has multiple digits")
-			fmt.Print("\n")
+			fmt.Println(i, " has multiple digits")
+
 		}
 		// if i%20 == 0 {
 		// 	fmt.Print("\n")
