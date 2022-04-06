bd8dfe7c93a56bce267d7771c7e3bf29abdd051b
turtlemurphy <turtlemurphy@gmail.com>
2022-04-05 22:52:04 -0500

    flipping switches

--- switch/switch.go
+++ switch/switch.go
@@ -8,19 +8,50 @@ import (
 func main() {
 	i := 2
 	fmt.Print("Write ", i, " as ")
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
+	fmt.Print("\n")
+	fmt.Print("\n")
+
+	for j := 1; j <= 10; j++ {
+
+		fmt.Print(j, " ")
+
+		switch j {
+		case 1:
+			fmt.Println(" one")
+		case 2:
+			fmt.Println(" two")
+		case 3:
+			fmt.Println(" three")
+		case 4:
+			fmt.Println(" four")
+		case 5:
+			fmt.Println(" five")
+		case 6:
+			fmt.Println(" six")
+		case 7:
+			fmt.Println(" seven")
+		case 8:
+			fmt.Println(" eight")
+		case 9:
+			fmt.Println(" nine")
+		case 10:
+			fmt.Println("ten")
+		}
+
+		if j%20 == 0 {
+			fmt.Print("\n")
+		}
 	}
+
+	fmt.Print("\n")
+	fmt.Print("\n")
 
 	switch time.Now().Weekday() {
 	case time.Saturday, time.Sunday:
+		fmt.Println(time.Now())
 		fmt.Println("It's the weekend")
 	default:
+		fmt.Println(time.Now())
 		fmt.Println("It's a weekday")
 	}
 
