8f6530a1d97ca70b995299f2bd6bd718ab2b842a
turtlemurphy <turtlemurphy@gmail.com>
2022-04-12 22:52:46 -0500

    ranging the ranges

--- range/range.go
+++ range/range.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{2, 3, 4}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	sum := 0
+
 	for _, num := range nums {
 		sum += num
+		fmt.Println("sum:", sum)
 	}
-	fmt.Println("sum:", sum)
+
+	fmt.Print("\n")
 
 	for i, num := range nums {
 		if num == 3 {
@@ -16,10 +19,15 @@ func main() {
 		}
 	}
 
+	fmt.Print("\n")
+
 	kvs := map[string]string{"a": "apple", "b": "banana"}
+
 	for k, v := range kvs {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
+
+	fmt.Print("\n")
 
 	for k := range kvs {
 		fmt.Println("key:", k)
