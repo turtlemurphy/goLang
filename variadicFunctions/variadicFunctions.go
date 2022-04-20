93b5c74d40f5c7fc2bfccc12c6224f7cccad88f7
turtlemurphy <turtlemurphy@gmail.com>
2022-04-19 22:15:27 -0500

    variadic functions

---
+++ variadicFunctions/variadicFunctions.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Print("\n")
+	fmt.Println(total)
+}
+func main() {
+
+	sum(1, 2)
+	sum(1, 2, 3)
+
+	nums := []int{1}
+
+	for j := 1; j <= 42; j++ {
+		sum(nums...)
+		nums = append(nums, j)
+	}
+}
