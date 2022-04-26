2f740c2565f02d7379beb096360d6641eb0a7a4d
turtlemurphy <turtlemurphy@gmail.com>
2022-04-25 23:48:31 -0500

    Adding more methods to my methods

--- methods/methods.go
+++ methods/methods.go
@@ -20,16 +20,56 @@ func (r rect) perim() int {
 func sieveOfEratosthenes(N int) (primes []int) {
 	b := make([]bool, N)
 	for i := 2; i < N; i++ {
-		if b[i] == true {
+		if b[i] {
 			continue
 		}
+
 		primes = append(primes, i)
+
 		for k := i * i; k < N; k += i {
 			b[k] = true
 		}
 	}
 	fmt.Println(primes)
 	return
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b uint) uint {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	fmt.Println(a)
+	return a
+}
+
+// Euler’s Totient Function
+func Phi(n uint) (result uint) {
+	// Initialize result as n
+	result = n
+
+	// Consider all prime factors of n and subtract their
+	// multiples from result
+	var p uint
+	for p = 2; p*p <= n; p++ {
+		// Check if p is a prime factor.
+		if n%p == 0 {
+			// If yes, then update n and result
+			for n%p == 0 {
+				n /= p
+			}
+			result -= (result / p)
+		}
+	}
+
+	// If n has a prime factor greater than sqrt(n)
+	// (There can be at-most one such prime factor)
+	if n > 1 {
+		result -= (result / n)
+	}
+	return result
 }
 
 func main() {
@@ -39,10 +79,22 @@ func main() {
 	fmt.Println("area: ", r.area())
 	fmt.Println("perim:", r.perim())
 
+	fmt.Println()
+
 	rp := &r
 	fmt.Println("area: ", rp.area())
 	fmt.Println("perim:", rp.perim())
 
-	sieveOfEratosthenes(10000)
+	fmt.Println()
 
+	sieveOfEratosthenes(420)
+
+	fmt.Println()
+
+	GCD(7231596374929284751, 69427)
+
+	fmt.Println()
+
+	p := Phi(8)
+	fmt.Println(p)
 }
