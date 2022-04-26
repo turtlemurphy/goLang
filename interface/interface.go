3908e21d5e767bf11e47098d6e05a6ce5ff05037
turtlemurphy <turtlemurphy@gmail.com>
2022-04-26 15:00:09 -0500

    interfacing the interface to the code interface

--- interface/interface.go
+++ interface/interface.go
@@ -5,18 +5,23 @@ import (
 	"math"
 )
 
+//no matter which struct is chosen both are a form of geometry and therefore the both have area and perimeter
 type geometry interface {
 	area() float64
 	perim() float64
 }
 
+//define rectangles
 type rect struct {
 	width, height float64
 }
+
+//define circles
 type circle struct {
 	radius float64
 }
 
+//define area and perim for rectangles
 func (r rect) area() float64 {
 	return r.width * r.height
 }
@@ -24,6 +29,7 @@ func (r rect) perim() float64 {
 	return 2*r.width + 2*r.height
 }
 
+//define area and perim for circles
 func (c circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
@@ -31,14 +37,16 @@ func (c circle) perim() float64 {
 	return 2 * math.Pi * c.radius
 }
 
+//defines measure funtion that takes geometrys as input
 func measure(g geometry) {
 	fmt.Println(g)
 	fmt.Println(g.area())
 	fmt.Println(g.perim())
 }
+
 func main() {
-	r := rect{width: 3, height: 4}
-	c := circle{radius: 5}
+	r := rect{width: 69, height: 420}
+	c := circle{radius: 42069}
 
 	measure(r)
 	measure(c)
