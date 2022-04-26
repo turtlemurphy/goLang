099f4e74be5d2189c54991014809c0abe8564720
turtlemurphy <turtlemurphy@gmail.com>
2022-04-25 23:14:05 -0500

    Adding more methods to my methods

--- methods/methods.go
+++ methods/methods.go
@@ -6,15 +6,34 @@ type rect struct {
 	width, height int
 }
 
+//func 1 - AREA
 func (r *rect) area() int {
 	return r.width * r.height
 }
 
+//func 2 - Referenced AREA
 func (r rect) perim() int {
 	return 2*r.width + 2*r.height
 }
 
+//func 3 - Prime Number Generator
+func sieveOfEratosthenes(N int) (primes []int) {
+	b := make([]bool, N)
+	for i := 2; i < N; i++ {
+		if b[i] == true {
+			continue
+		}
+		primes = append(primes, i)
+		for k := i * i; k < N; k += i {
+			b[k] = true
+		}
+	}
+	fmt.Println(primes)
+	return
+}
+
 func main() {
+
 	r := rect{width: 10, height: 5}
 
 	fmt.Println("area: ", r.area())
@@ -23,4 +42,7 @@ func main() {
 	rp := &r
 	fmt.Println("area: ", rp.area())
 	fmt.Println("perim:", rp.perim())
+
+	sieveOfEratosthenes(10000)
+
 }
